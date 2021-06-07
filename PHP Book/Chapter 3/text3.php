f0d68956e16721d372e9b45b36a340973b821cfb
DJBooker2 <problemchildprogramming@gmail.com>
2021-06-07 10:18:20 -0400

    -- Done for Today -- DJ Booker

--- PHP Book/Chapter 3/text3.php
+++ PHP Book/Chapter 3/text3.php
@@ -8,7 +8,8 @@
 $month = "March";
 
 if ($month == "March") {
-    echo "It's springtime";
+    echo "It's springtime<br>";
+    echo "<hr />";
 }
 ?>
 
@@ -21,19 +22,46 @@ if ($a == $b) {
     echo "1";
 }
 if ($a === $b) {
-    echo "2";
+    echo "2<br>";
+    echo "<hr/>";
 }
 ?>
 
 <?php
-    // The inequality and not-identical operators
-    $a = "1000";  
-    $b = "+1000";  
-    
-    if ($a != $b)  echo "1";  
-    if ($a !== $b) echo "2";
+// The inequality and not-identical operators
+$a = "1000";
+$b = "+1000";
+
+if ($a != $b)  echo "1";
+if ($a !== $b) echo "2";
+echo "<br>";
+echo "<hr/>";
 ?>
 
 <?php
+$a = 2;
+$b = 3;
+
+if ($a > $b) {
+    echo "$a is greater than $b<br>";
+}
+if ($a < $b) {
+    echo "$a is less than $b<br>";
+}
+if ($a >= $b) {
+    echo "$a is greater than or equal too $b<br>";
+}
+if ($a <= $b) {
+    echo "$a is less than or equal to $b<br>";
+}
+echo "<hr/>";
+?>
+
+<?php
+    $a = 1; $b = 0;
     
+    echo ($a AND $b) . "<br>";
+    echo ($a or $b) . "<br>";
+    echo ($a XOR $b) . "<br>";
+    echo !$a . "<br>";
 ?>
