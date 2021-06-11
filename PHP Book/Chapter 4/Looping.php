8080a394b8c9648740a475e202079c077894167c
DJBooker2 <problemchildprogramming@gmail.com>
2021-06-11 11:46:48 -0400

    -- Working on book -- DJ Booker

--- PHP Book/Chapter 4/Looping.php
+++ PHP Book/Chapter 4/Looping.php
@@ -31,7 +31,67 @@ while ($fuel > 1) {
     // Keep driving....
     echo "There's enough fuel";
 } */
-echo "There's enough fuel";
+echo "There's enough fuel";  // Result of the while loop above
 echo "<hr />";
 echo "<br>";
 ?>
+
+<!--While loop to print the 12 times tables-->
+<?php
+$count = 1;
+
+while ($count <= 12) {
+    echo "$count time 12 is " . $count * 12 . "<br>";
+    ++$count;
+}
+echo "<hr />";
+echo "<br>";
+?>
+
+<!--A do...while loop example-->
+<?php
+$count2 = 1;
+do {
+    echo "$count2 time 12 is " . $count2 * 12 . "<br>";
+} while (++$count2 <= 12);
+echo "<hr />";
+echo "<br>";
+?>
+
+<!--12 times table using a for loop-->
+<?php
+for ($count3 = 1; $count3 <= 12; ++$count3) {
+    echo "$count3 time 12 is " . $count3 * 12 . "<br>";
+}
+echo "<hr />";
+echo "<br>";
+?>
+
+<!-- The continue statement-->
+<!--Trapping division-by-zero errors using continue-->
+<?php
+$j = 10;
+
+while ($j > -10) {
+    $j--;
+    if ($j == 0) {
+        continue;
+    }
+    echo (10 / $j) . "<br>";
+}
+echo "<hr />";
+echo "<br>";
+?>
+
+<!--Returning floating-point numbers-->
+<?php
+$a = 56;
+$b = 12;
+$c = $a / $b;
+
+echo $c;
+echo "<hr />";
+echo "<br>";
+?>
+
+// Page 90
